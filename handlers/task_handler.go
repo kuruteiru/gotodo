0be26c1427090795787490f0b749dd8cb00db244
kuruteiru <martin.krutil@email.cz>
2025-01-23 16:20:22 +0100

    view task related pages

--- handlers/task_handler.go
+++ handlers/task_handler.go
@@ -1,11 +1,27 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/kuruteiru/gotodo/models"
 	"github.com/kuruteiru/gotodo/renderer"
 )
+
+func ViewTask(w http.ResponseWriter, r *http.Request) {
+	p := r.PathValue("page")
+	p = "task/"+p
+	renderer.RenderTemplate(w, p, nil)
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		w.WriteHeader(http.StatusConflict)
+	}
+	f := r.PostForm
+	fmt.Printf("%+v", f)
+	// renderer.RenderTemplate(w, p, nil)
+}
 
 func ViewTodolist(w http.ResponseWriter, r *http.Request) {
 	pd := &renderer.PageData{
