749a832cff5f34dc90233e9aa9842f1e69f900b7
kuruteiru <martin.krutil@email.cz>
2025-02-01 18:33:32 +0100

    better error handeling + thread safety with RWMutex

--- db/db.go
+++ db/db.go
@@ -2,14 +2,17 @@ package db
 
 import (
 	"encoding/csv"
+	"errors"
 	"fmt"
-	// "math/rand/v2"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/kuruteiru/gotodo/models"
 )
+
+var mutex sync.RWMutex
 
 func Main() {
 	t, err := ReadTasks(1)
@@ -30,84 +33,140 @@ func Main() {
 	// models.PrintTasksTable(t)
 }
 
-// todo: handle errors
 func ReadTasks(id uint64) ([]models.Task, error) {
 	path := fmt.Sprintf("db/tasks/%v.csv", id)
+
+	mutex.RLock()
+	defer mutex.RUnlock()
+
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to open file %s: %w", path, err)
 	}
 	defer f.Close()
 
 	r := csv.NewReader(f)
 	records, err := r.ReadAll()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed reading csv file %s: %w", path, err)
 	}
 
 	var tasks []models.Task
+	var errs []error
+
 	for i, record := range records {
 		if i == 0 {
 			continue
 		}
 
-		id, _ := strconv.ParseUint(record[0], 10, 64)
-		name := record[1]
-		description := record[2]
-		priority, _ := strconv.ParseUint(record[3], 10, 8)
-		created, _ := time.Parse(time.DateTime, record[4])
-		var completed *time.Time
-		if record[5] != "" {
-			c, _ := time.Parse(time.DateTime, record[5])
-			completed = &c
+		task, err := parseTask(record)
+		if err != nil {
+			errs = append(errs, fmt.Errorf("can't read record %v: [%w]", i, err))
+			continue
 		}
 
-		tasks = append(tasks, models.Task{
-			ID:          id,
-			Name:        name,
-			Description: description,
-			Priority:    models.TaskPriority(priority),
-			Created:     created,
-			Completed:   completed,
-		})
+		if task == nil {
+			continue
+		}
+
+		tasks = append(tasks, *task)
+	}
+
+	if len(tasks) == 0 {
+		return nil, errors.New("no valid tasks found")
+	}
+	
+	if errs != nil {
+		return tasks, fmt.Errorf("invalid records: [%w]", errors.Join(errs...))
 	}
 
 	return tasks, nil
 }
 
-// todo: handle errors
+//todo: write into tmp file instead
 func WriteTasks(id uint64, tasks []models.Task) error {
 	path := fmt.Sprintf("db/tasks/%v.csv", id)
+
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		fmt.Printf("err: %v\n", err.Error())
-		return err
+		return fmt.Errorf("failed to open file %s: %w", path, err)
+
 	}
 	defer f.Close()
 
 	w := csv.NewWriter(f)
 	defer w.Flush()
 
-	for _, t := range tasks {
-		record := []string{
-			strconv.FormatUint(t.ID, 10),
-			t.Name,
-			t.Description,
-			strconv.FormatUint(uint64(t.Priority), 10),
-			t.Created.Format(time.DateTime),
-			// t.Created.Add(time.Duration(rand.IntN(100)+1) * time.Second).Format(time.DateTime),
-		}
-
-		if t.Completed != nil {
-			record = append(record, t.Completed.Format(time.DateTime))
-		} else {
-			record = append(record, "")
-		}
-
-		if err := w.Write(record); err != nil {
-			return err
+	var errs []error
+	for i, t := range tasks {
+		if err := w.Write(formatTask(t)); err != nil {
+			errs = append(errs, fmt.Errorf("couldn't write record %v: [%w]", i, err))
+			continue
 		}
 	}
 
 	return nil
+}
+
+func parseTask(record []string) (*models.Task, error) {
+	if len(record) > 6 {
+		return nil, fmt.Errorf("invalid record: [%v]", record)
+	}
+
+	id, err := strconv.ParseUint(record[0], 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("invalid id: [%w]", err)
+	}
+
+	name := record[1]
+	description := record[2]
+
+	priority, err := strconv.ParseUint(record[3], 10, 8)
+	if err != nil {
+		return nil, fmt.Errorf("invalid priority: [%w]", err)
+	}
+
+	created, err := time.Parse(time.DateTime, record[4])
+	if err != nil {
+		return nil, fmt.Errorf("invalid creation date: [%w]", err)
+	}
+
+	var completed *time.Time
+	if record[5] != "" {
+		if c, err := time.Parse(time.DateTime, record[5]); err != nil {
+			return nil, fmt.Errorf("invalid completion date: [%w]", err)
+		} else {
+			completed = &c
+		}
+	}
+
+	return &models.Task{
+		ID:          id,
+		Name:        name,
+		Description: description,
+		Priority:    models.TaskPriority(priority),
+		Created:     created,
+		Completed:   completed,
+	}, nil
+}
+
+func formatTask(task models.Task) []string {
+	record := []string{
+		strconv.FormatUint(task.ID, 10),
+		task.Name,
+		task.Description,
+		strconv.FormatUint(uint64(task.Priority), 10),
+		task.Created.Format(time.DateTime),
+	}
+
+	if task.Completed != nil {
+		record = append(record, task.Completed.Format(time.DateTime))
+	} else {
+		record = append(record, "")
+	}
+
+	return record
 }
